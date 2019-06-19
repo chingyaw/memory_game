54b4b42a8eb7791abb8a7e20008cef415397dc6a
chingyaw <cywang2011@gmail.com>
2019-06-18 18:19:42 -0700

    Remove unnecessary comments anad functions

--- js/app.js
+++ js/app.js
@@ -5,41 +5,68 @@
  *      (2). Reset all properties to default
  *  2. Playing
  *      (1). Matched or not
- *      (2). Flip card
  *
  * [Timer and Score]
  *  1. Set/Get playing time
  *  2. Set/Get score
  */
 
-
-/*
- * class: match
- * class: open show
- */
-
-/*
- * Create a list that holds all of your cards
- */
-var openedCardCount = 0; // track current run's open card (0 means no open card, max is 2). if it hits 2 but two cards are not matched, reset it to 0
-var playing = false;
-var openedCards = [];
-var matchedPairs = 0;
-var waiting = false;
-var flipCards = false;
-var startTime = 0;
-var endTime = 0;
-var openCount = 0;
-
-var playTimer;
-var totalTimeInSeconds = 0;
-var hour = '00';
-var minute = '00';
-var second = '00';
-
+// Create a list that holds all of the cards
 const iconList = ['fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-bolt', 'fa-paper-plane-o', 'fa-cube', 'fa-anchor',
     'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-bolt', 'fa-paper-plane-o', 'fa-cube', 'fa-anchor'];
-let cards = {};//[]; // an array of objects, {id: int, position: int, matched: boolean, opened: boolean}
+let cards = {}; // an array of objects, {id: int, position: int, matched: boolean, opened: boolean}
+
+let playing = false; // a flag that makes sure some variables are initialized just once
+let openedCards = []; // track current open cards. If it hits 2 but two cards are not matched, reset it to []
+let matchedPairs = 0; // max = 8, using this variable to determine if all the cards are matched
+let waiting = false; // user can't open new card while in waiting state (waiting = true when two cards are not matched and waiting for 'close' cards
+let openCount = 0; // total open count. it's used for determining player's performance (ranking)
+let moveCount = 0;
+
+// Timer
+let playTimer; // to display user's playing time
+let totalTimeInSeconds = 0; // total time in seconds and will be converted to hh:mm:ss format
+let hours = '00';
+let minutes = '00';
+let seconds = '00';
+
+
+const pattern = /card\d+/; // card ids, i.e. card0 - card15
+const restartElement = document.querySelector('.restart');
+const deckElement = document.querySelector('.deck');
+const childNodes = deckElement.querySelectorAll('.card');
+const moveCountElement = document.querySelector('.moves');
+const star0 = document.querySelector('#star0');
+const star1 = document.querySelector('#star1');
+const star2 = document.querySelector('#star2');
+const timerElement = document.querySelector('.timer');
+const resultModal = document.querySelector(".modal");
+const replayButton = document.querySelector(".replay");
+const cancelButton = document.querySelector(".cancel");
+
+// set default values
+function initialization() {
+    star0.className = 'fa fa-star';
+    star1.className = 'fa fa-star';
+    star2.className = 'fa fa-star';
+
+    openedCards = [];
+    matchedPairs = 0;
+    waiting = false;
+    openCount = 0;
+    moveCount = 0;
+    playing = false;
+
+    totalTimeInSeconds = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+
+    deckElement.addEventListener('click', openCard);
+    restartElement.addEventListener('click', startGame);
+    replayButton.addEventListener('click', replay);
+    cancelButton.addEventListener('click', toggleModal);
+}
 
 /*
  * Display the cards on the page
@@ -63,34 +90,15 @@ function shuffle(array) {
     return array;
 }
 
+function compareTwoCards(card1, card2) {
+    return cards[card1]['icon'] === cards[card2]['icon'];
+}
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-const pattern = /card\d+/;
-const restartElement = document.querySelector('.restart');
-const deckElement = document.querySelector('.deck');
-const childNodes = deckElement.querySelectorAll('.card');
-const moveCountElement = document.querySelector('.moves');
-const starElement = document.querySelector('.stars');
-const starNodes = starElement.querySelectorAll('li');
-
-const timerElement = document.querySelector('.timer');
-const resultModal = document.querySelector(".modal");
-const closeButton = document.querySelector(".close-button");
-const replayButton = document.querySelector(".replay");
-const cancelButton = document.querySelector(".cancel");
-
-// Reference: https://sabe.io/tutorials/how-to-create-modal-popup-box
-function toggleModal() {
-    resultModal.classList.toggle("show-modal");
+function convertSecondsToHHMMSS(seconds) {
+    hours = parseInt(totalTimeInSeconds / 3600);
+    minutes = parseInt((totalTimeInSeconds - hours *3600) / 60);
+    seconds = parseInt(totalTimeInSeconds - hours * 3600 - minutes * 60);
+    return `${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
 }
 
 function openCard(evt) {
@@ -100,53 +108,45 @@ function openCard(evt) {
         const selectedCard = cards[selectedCardId];
 
         if(!playing) {
-            startTime = performance.now();
             playing = true;
-            
+            // start timer
             playTimer = setInterval(function () {
                 totalTimeInSeconds += 1;
-
-                hour = parseInt(totalTimeInSeconds / 3600);
-                minute = parseInt((totalTimeInSeconds - hour*3600) / 60);
-                second = parseInt(totalTimeInSeconds - hour * 3600 - minute * 60)
-                timerElement.textContent = `${("0" + hour).slice(-2)}:${("0" + minute).slice(-2)}:${("0" + second).slice(-2)}`;
+                timerElement.textContent = convertSecondsToHHMMSS(totalTimeInSeconds);
             }, 1000);
-            
-            
         }
 
-        if (!selectedCard['opened'] && openedCardCount < 2) {
+        // the selected card is not opened and count < 2
+        if (!selectedCard['opened'] && openedCards.length < 2) {
             openCount += 1;
-            moveCount = parseInt(openCount / 2)
+            moveCount = parseInt(openCount / 2);
             moveCountElement.textContent = (moveCount > 1)? moveCount + ' moves': moveCount + ' move';
-            if (moveCount > 8 && moveCount <= 16) {
-                // const elem2 = document.querySelector('#star2');
-                // if (elem2) {
-                //     starElement.removeChild(elem2);
-                // }
-                const star2 = document.querySelector('#star2');
-                star2.style.display = 'none';
-            } else if (moveCount > 16) {
-                // const elem1 = document.querySelector('#star1');
-                // if (elem1) {
-                //     starElement.removeChild(elem1);
-                // }
-                const star1 = document.querySelector('#star1');
-                star1.style.display = 'none';
+
+            // 1 2 3 4 5 6 7 8 9 10
+            // 0 0 0 0 0 0 0 0 1
+            if (8 < moveCount && moveCount <=16) {
+                star2.className = 'fa fa-star-half-o';
+            } else if (16 < moveCount && moveCount <=24) {
+                star2.className = 'fa fa-star-o';
+            } else if (24 < moveCount && moveCount <=32) {
+                star1.className = 'fa fa-star-half-o';
+            } else if (32 < moveCount && moveCount <=40) {
+                star1.className = 'fa fa-star-o';
+            } else if (40 < moveCount && moveCount <=48) {
+                star0.className = 'fa fa-star-half-o';
+            } else if (moveCount > 48) {
+                star0.className = 'fa fa-star-o';
             }
-            // this card is not opened yet
 
             selectedCard['opened'] = true;
             openedCards.push(target);
-            openedCardCount += 1;
-
 
             target.className = 'card open show';
 
             const currentTargetIconElement = target.querySelector('i');
             currentTargetIconElement.className = 'fa ' + selectedCard['icon'];
 
-            if (openedCardCount === 2) {
+            if (openedCards.length === 2) {
                 // call match function
                 // if not matched, reset openedCardCount to 0 and flip both cards
                 if (compareTwoCards(openedCards[0].id, openedCards[1].id)) {
@@ -159,10 +159,8 @@ function openCard(evt) {
                     matchedPairs += 1;
 
                     if (matchedPairs === 8) {
-                        endTime = performance.now();
                         clearInterval(playTimer);
                         toggleModal();
-                        // console.log('Playing time: ', endTime - startTime, ' Open count: ', openCount);
                     }
                 } else {
                     // reset opened cards' properties
@@ -177,89 +175,52 @@ function openCard(evt) {
                         openedCards = [];
                         waiting = false;
                     }, 1000);
-
-
-
-
                 }
-                openedCardCount = 0;
             }
         }
-        // card has been opened, do nothing
-    } else {
-        // not a card element, do nothing
     }
-
-
 }
 
-function compareTwoCards(card1, card2) {
-    return cards[card1]['icon'] === cards[card2]['icon'];
-}
+function startGame() {
 
-function resetGame() {
-
-    openedCardCount = 0;
-    openedCards = [];
-    matchedPairs = 0;
-    waiting = false;
-    flipCards = false;
-    startTime = 0;
-    endTime = 0;
-    openCount = 0;
-    playing = false;
-
-    totalTimeInSeconds = 0;
-    hour = 0;
-    minute = 0;
-    second = 0;
-    clearInterval(playTimer);
-    timerElement.textContent = `${("0" + hour).slice(-2)}:${("0" + minute).slice(-2)}:${("0" + second).slice(-2)}`;
-    // Assign an id for each card
-    for (let i = 0; i< 16; i++) {
-        childNodes[i].id = 'card' + i.toString();
+    // stop timer
+    if (playTimer) {
+        clearInterval(playTimer);
     }
-
-
-    for (let i = 0; i< 3; i++) {
-        starNodes[i].id = 'star' + i.toString();
-    }
-
-    // reset star rating
-    const star0 = document.querySelector('#star0');
-    star0.style.display = 'inline';
-
-    const star1 = document.querySelector('#star1');
-    star1.style.display = 'inline';
-
-    const star2 = document.querySelector('#star2');
-    star2.style.display = 'inline';
-
-    // reset moves
-    moveCountElement.textContent = 0;
 
     // reset opened cards
     var shfulledIcon = shuffle(iconList);
 
-    for (i = 0; i < 16; i++) {
-        //cards.push({'id': 'card' + i.toString(), 'position': shfulledPosition[i], 'matched': false, 'opened': false});
-        const cardId = 'card' + i.toString();
-        cards[cardId] = {'icon': shfulledIcon[i], 'matched': false, 'opened': false};
+    // Assign an id for each card
+    for (let i = 0; i< 16; i++) {
+        childNodes[i].id = 'card' + i.toString();
+        cards[childNodes[i].id] = {'icon': shfulledIcon[i], 'matched': false, 'opened': false};
         childNodes[i].className = 'card';
     }
+
+    // reset move
+    moveCountElement.textContent = 0;
+
+    // reset variables to default values
+    initialization();
+    timerElement.textContent = convertSecondsToHHMMSS(0);
+}
+
+function generateResult() {
+    const rankingResultElement = document.querySelector('.ranking-result');
+    rankingResultElement.textContent = (moveCount - 1) / ;
+    toggleModal();
+}
+
+// Reference: https://sabe.io/tutorials/how-to-create-modal-popup-box
+function toggleModal() {
+    resultModal.classList.toggle("show-modal");
 }
 
 function replay(){
     toggleModal();
-    resetGame();
+    startGame();
 }
 
-
-resetGame();
-
-// document.body.appendChild(deckElement);
-deckElement.addEventListener('click', openCard);
-restartElement.addEventListener('click', resetGame);
-// closeButton.addEventListener("click", toggleModal);
-replayButton.addEventListener('click', replay);
-cancelButton.addEventListener('click', toggleModal);
+startGame();
+generateResult();
